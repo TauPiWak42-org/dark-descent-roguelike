8b2e18ae70602e4101b4c1353618bf5671a37ffd
TauPiWak42 <deswerty2431@gmail.com>
2026-08-28 04:01:09 +1000

    Create main.js

---
+++ src/main.js
@@ -0,0 +1,11 @@
+import { Game } from './core/Game.js';
+import { Player } from './entities/Player.js';
+import { Camera } from './systems/Camera.js';
+import { MapGenerator } from './systems/MapGenerator.js';
+import { HUD } from './ui/HUD.js';
+import { EnemyManager } from './systems/EnemyManager.js';
+import { CombatSystem } from './systems/CombatSystem.js';
+import { ParticleSystem } from './systems/ParticleSystem.js';
+
+// Инициализация игры
+const
