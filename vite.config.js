30ba893a81d944f1757512dcf1c85e0aec64da3d
TauPiWak42 <deswerty2431@gmail.com>
2026-08-28 03:46:38 +1000

    Create vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,21 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  base: '/dark-descent-roguelike/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    minify: 'esbuild',
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined
+      }
+    }
+  },
+  server: {
+    port: 3000,
+    open: true,
+    host: true
+  }
+});
